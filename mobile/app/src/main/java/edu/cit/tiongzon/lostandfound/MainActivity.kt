51453abf2ef65a765b68ba6f124356b044666d6f
LAB13_USER ON WS37 <L13Y09W37@LabHCI.net>
2026-03-28 09:11:13 +0800

    feat(home): implement home screen and navigation graph

---
+++ mobile/app/src/main/java/edu/cit/tiongzon/lostandfound/MainActivity.kt
@@ -0,0 +1,27 @@
+package edu.cit.tiongzon.lostandfound
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import edu.cit.tiongzon.lostandfound.ui.navigation.NavGraph
+import edu.cit.tiongzon.lostandfound.ui.theme.LostAndFoundTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LostAndFoundTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    val navController = rememberNavController()
+                    NavGraph(navController = navController)
+                }
+            }
+        }
+    }
+}
