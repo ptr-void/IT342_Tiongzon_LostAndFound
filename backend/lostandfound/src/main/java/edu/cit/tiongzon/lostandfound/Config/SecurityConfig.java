d5950567b7db26565538c9bc2f4197e554653f73
LAB13_USER ON WS37 <L13Y09W37@LabHCI.net>
2026-04-11 09:15:21 +0800

    chore(backend): update security configuration

--- backend/lostandfound/src/main/java/edu/cit/tiongzon/lostandfound/Config/SecurityConfig.java
+++ backend/lostandfound/src/main/java/edu/cit/tiongzon/lostandfound/Config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
-                        .requestMatchers("/users/**", "/auth/**", "/error").permitAll()
+                        .requestMatchers("/users/**", "/auth/**", "/items/**", "/error").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
